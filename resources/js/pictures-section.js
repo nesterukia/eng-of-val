f24dae4a4e9c6fcea9cfc5e53c35655fca2c989e
Daniil Nesteruk <nesteruk.da@edu.spbstu.ru>
2024-06-20 16:36:06 +0300

    added picture__buttons

--- resources/js/pictures-section.js
+++ resources/js/pictures-section.js
@@ -2,9 +2,44 @@ const picturesSection = document.querySelector(".pictures");
 const inputs = document.querySelectorAll(".pictures__input");
 const contents = document.querySelectorAll(".content");
 const labels = document.querySelectorAll(".pictures__label");
+const leftButton = document.querySelector(".pictures__left-button");
+const rightButton = document.querySelector(".pictures__right-button");
 let runAnimation = true;
 let inputNum = 0;
 
+
+
+leftButton.addEventListener("click", e =>{
+    runAnimation = false;
+    console.log("before", inputNum);
+    if(inputNum == 0){
+        inputNum = 5;
+    } else inputNum--;
+    inputs.forEach(input => {
+        input.checked = false;
+    });
+    inputs[inputNum].checked = true;
+    console.log("after", inputNum);
+    setTimeout(()=>{
+        runAnimation = true;
+    }, 5000);
+})
+
+rightButton.addEventListener("click", e =>{
+    runAnimation = false;
+    console.log("before", inputNum);
+    if(inputNum == 5){
+        inputNum = 0;
+    } else inputNum++;
+    inputs.forEach(input => {
+        input.checked = false;
+    });
+    inputs[inputNum].checked = true;
+    console.log("after", inputNum);
+    setTimeout(()=>{
+        runAnimation = true;
+    }, 5000);
+})
 picturesSection.addEventListener("mouseover", e =>{
     e.target.style.cursor = 'pointer';
     circles.forEach(circle => {
